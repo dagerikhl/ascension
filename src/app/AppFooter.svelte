305435b244e52aedbd3b2725b61298442b731d85
Løvgren, Dag Erik <dagerikhl@gmail.com>
2021-11-09 20:04:16 +0100

    Add Miles logo and version number to footer

--- src/app/AppFooter.svelte
+++ src/app/AppFooter.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+  import { version } from '../../package.json';
 </script>
 
-<footer class="AppFooter">&copy; Miles/Dag Erik Løvgren 2021-(present)</footer>
+<footer class="AppFooter">
+  <div class="group">
+    <div class="group"><img class="logo" src="assets/logos/miles.svg" alt="Miles" /></div>
+
+    <div class="group">&copy; Miles/Dag Erik Løvgren 2021-(present)</div>
+  </div>
+
+  <div class="group">v{version}</div>
+</footer>
 
 <style lang="scss">
   .AppFooter {
@@ -11,5 +20,16 @@
     justify-content: space-between;
     align-items: center;
     padding: 0 2rem;
+
+    .group {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+    }
+
+    .logo {
+      height: 64px;
+      width: 64px;
+    }
   }
 </style>
